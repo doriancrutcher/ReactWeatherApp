e9ab4214764892b16e8c759e82a7959b33540a08
Dorian Crutcher <doriancrutcher@Dorians-MBP.fios-router.home>
2020-08-11 14:13:49 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Input from './Container/Input/Input'
+import DataField from './Container/DataField/DataField'
+import Header from './Component/Header/Header';
 
-function App() {
+class App extends Component {
+  constructor(props){
+          super(props)
+          this.state={
+              weatherTemp:'--',
+              location:'--',
+              condition:'Clear'
+          }
+      
+  }
+
+  WeSearch=(textIn)=>{
+    let City=textIn
+    let FullApi=`https://api.openweathermap.org/data/2.5/weather?q=${City}&appid=f6760ba909cd12408a11186c3c81c69d`
+    fetch(FullApi)
+    .then(res=>res.json())
+    .then(result=>{console.log(result.weather);this.setState({
+      weatherTemp:Math.floor(9/5*(result.main.temp-273)+32),
+      location:City,
+      condition:result.weather[0].main
+      }
+    )})
+    .catch(error=>{
+
+      this.setState({
+        weatherTemp:'--',
+        location:'--',
+        condition:'World'
+      })
+      return Promise.reject()
+    }
+    )
+
+    
+  }
+
+  componentDidUpdate(){
+    console.log(this.state.weatherTemp)
+  }
+
+
+
+  render(){
+    
   return (
+    <div className={"BackDrop"+' '+this.state.condition}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Input WeSearch={this.WeSearch}/>
+      <div class="DataBlock">
+      <DataField Temperature={this.state.weatherTemp} Location={this.state.location}/>
+      </div>
+      
+    </div>
     </div>
   );
+  }
 }
 
 export default App;
